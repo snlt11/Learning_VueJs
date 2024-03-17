b06554f0f5b5620bd15d56a20defa355238c3788
Sai Nay Lin Thar <sainaylinthar@gmail.com>
2024-03-17 11:35:33 +0630

    Day 2 : VueCli

---
+++ VueCli/src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <h1 ref="title">{{ title }}</h1>
+  <input type="text" ref="name" /><br /><br />
+  <button @click="clickHandler">Click Me</button><br /><br />
+  <div v-if="showModal">
+    <Modal
+      :header="header"
+      :content="content"
+      :theme="theme"
+      @closeModal="showModal = false"
+    >
+      <h1>testing</h1>
+      <p>Welcome tester</p>
+      <template v-slot:links>
+        <a href="http://www.facebook.com">To Facebook</a>
+        <a href="http://www.google.com">To Google</a>
+      </template>
+    </Modal>
+  </div>
+  <button @click="showModal = true">show Modal</button>
+  <div v-if="showModalTwo">
+    <Modal theme="delete" @closeModal="showModalTwo = false">
+      <h1>Delete</h1>
+      <p>Deleted tester</p>
+    </Modal>
+  </div>
+  <button @click="showModalTwo = true">Delete</button>
+</template>
+
+<script>
+import Modal from "./components/Modal.vue";
+export default {
+  data() {
+    return {
+      title: "testing",
+      header: "header for success",
+      content: "content for success",
+      theme: "success",
+      showModal: false,
+      showModalTwo: false,
+    };
+  },
+  components: {
+    Modal,
+  },
+  methods: {
+    clickHandler() {
+      this.$refs.name.focus();
+      this.$refs.title.textContent = "ASUS";
+    },
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  color: blue;
+}
+</style>
---
+++ VueCli/src/components/Modal.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="backdrop" @click.self="closeModal">
+    <div
+      class="modal"
+      :class="{ success: theme == 'success', delete: theme == 'delete' }"
+    >
+      <slot>
+        <h1>Default</h1>
+      </slot>
+      <slot name="links"/>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["header", "content", "theme"],
+  methods: {
+    closeModal(){
+      this.$emit('closeModal')
+    }
+
+  }
+};
+</script>
+
+<style scoped>
+.backdrop {
+  position: fixed;
+  top: 0;
+  background: rgb(0, 0, 0, 0.5);
+  width: 100%;
+  height: 100%;
+}
+.modal {
+  width: 400px;
+  padding: 20px;
+  margin: 100px auto;
+  background-color: #fff;
+  border-radius: 10px;
+}
+p {
+  font-style: normal;
+}
+.success {
+  background-color: green;
+  color: #fff;
+}
+.delete {
+  background-color: crimson;
+  color: #fff;
+}
+</style>
